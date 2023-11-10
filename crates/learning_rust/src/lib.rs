e97104cab42d2c682f20c5f63d62362330d3bb84
james.rocker <james.rocker@sainsburys.co.uk>
2023-11-10 19:58:50 +0000

    more comments, replacing some strings

--- crates/learning_rust/src/lib.rs
+++ crates/learning_rust/src/lib.rs
@@ -1,30 +1,34 @@
-// as this file is a lib, this defines
+// as this file is a lib, which means it's a library file meant to be imported
+// while if this were a main.rs, it'd be an executable
 
 fn do_a_thing() {
     // rust functions are by default private. That's pretty cool
-    println!("Hello, a different thing!");
+    println!("Goodbye moon");
 }
 
 pub fn encapsulate() {
-    println!("Hello, world!");
+    // we can make functions public which means the code can be imported elsewhere
+    println!("Hello world!");
     do_a_thing();
 }
 
-// we aren't using this function so commenting it out so rust compiler doesn't complain
+// we weren't using this function within the module so we mark the code as dead so the compiler
+// doesn't complain. do_a_thing doesn't need it because it's used within another function
 #[allow(dead_code)]
-pub fn another_function(x: i32) -> i32 {
+fn another_function(x: i32) -> i32 {
     println!("The value of x is: {}", x);
     return x
 }
 
 pub fn print_labeled_measurement(value: i32, unit_label: char) -> String {
+    // some string concatenation
     return format!("{}{}", value, unit_label)
 }
 
 pub fn optional_args(string_option: Option<String>, second_string_option: Option<String>) -> String {
     // by default, variables are immutable however, we can specify mutability and then pass default values
-    let a_thing = string_option.unwrap_or("aghhhh, you didn't give an argument. ".parse().unwrap());
-    let found_unit = second_string_option.unwrap_or("oh no, you also didn't specify the second argument".parse().unwrap());
-    format!("{}{}", a_thing, found_unit) // implicit returns
+    let a_thing = string_option.unwrap_or("Boo! You didn't give an argument.".parse().unwrap());
+    let found_unit = second_string_option.unwrap_or("Oh no, you also didn't specify the second argument".parse().unwrap());
+    format!("{} {}", a_thing, found_unit) // implicit returns are allowed
 }
 
