5c05de5d8e7305a09d5d1b7d7add6d41f5bc5d37
james.rocker <james.rocker@sainsburys.co.uk>
2024-01-14 20:47:03 +0000

    updating my_first_library to following an actual tutorial

---
+++ crates/following_rust_tutorial/readme.md
@@ -0,0 +1,12 @@
+# Learnings
+
+This crate is going to be following "The Rust Programming Language 2nd Edition".
+
+## Notes
+
+These commands might be obvious to someone using the language on a daily basis. I do not though.
+`cargo check` - checks to see if the code compiles but doesn't build anything
+`cargo fmt` - auto formats `bin` and `lib` files with rustfmt
+
+## Commands
+`cargo run --bin guessing_game` - runs the guessing game program
---
+++ crates/following_rust_tutorial/src/bin/guessing_game.rs
@@ -0,0 +1,40 @@
+use std::io; // bringing the standard library into the programs scope
+use std::cmp::Ordering;
+// standard library allows for easy comparison between two objects
+use rand::Rng;  // bringing in a third part library for random number generation
+
+pub fn main() {
+    println!("Starting guessing number game!");
+    let secret_number = rand::thread_rng().gen_range(1..100); // initiates a generator for a range
+
+    loop {  // allows us to keep guessing the number until the user guesses the correct answer
+        println!("Please guess your number between 1 and 100");
+
+        let mut guess = String::new();  // initialises a mutable string variable
+
+        io::stdin() // calls the standard input
+            .read_line(&mut guess)// receives the users input and stores it to guess
+            .expect("Failed to read the line");  // this handles the error handling of the input
+
+        // takes the defined string object. Two objects with this variable name exist now.
+        let guess: u32 = match guess // Match means this doesn't crash
+            .trim()  // removes leading and trailing whitespace
+            .parse() { // parses the string into another type
+        Ok(num) => num,  // on the expression being okay, returns okay value with the number
+        Err(_) => continue,  // on error the underscore catches all errors and then restarts the loop
+        };
+
+        // This
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"); // when the correct number is guessed line is printed and loop is broken
+                break;
+            }
+        }
+
+
+        println!("You failed to guess the number. You guessed: {guess}.")
+    }
+}
